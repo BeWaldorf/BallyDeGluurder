9565114050da3d8191a2ecc0e3ccc3858b349081
Jordy Neefs <jordyneefs@telenet.be>
2023-12-20 22:49:38 +0100

    Update motor driver

--- Embedded/Motor/src/main.cpp
+++ Embedded/Motor/src/main.cpp
@@ -19,12 +19,17 @@ const String MQTT_CLIENTID = "ESP32-" + String(random(0xffff), HEX);
 
 const int LED_PIN = 2;
 const int BATTERY_PIN = 34;
-const float R1 = 1000000; // 1MΩ
-const float R2 = 560000;  // 560KΩ
+const float R2 = 1000000.0; // 1MΩ
+const float R1 = 560000.0;  // 560KΩ
+const float VOLTAGE_DIVIDER = (R1 + R2) / R2;
 const float BATTERY_MAX_VOLTAGE = 4.8;
+const float BATTERY_MIN_VOLTAGE = 3.6;
+const float ADC_RESOLUTION = 4095.0;
+
+const float MINUTES_DRIVING_PER_PERCENTAGE = 2.1;
 
 const int MOTOR1_ENA = 21;
-const int MOTOR1_IN1 = 22;
+const int MOTOR1_IN1 = 5;
 const int MOTOR1_IN2 = 23;
 
 const int MOTOR2_IN3 = 12;
@@ -36,27 +41,45 @@ const int PWM_RESOLUTION = 8;
 const int MOTOR_LEFT_PWM_CHANNEL = 0;
 const int MOTOR_RIGHT_PWM_CHANNEL = 1;
 
-bool newMQTTMessage = false;
-char direction = 's'; // Standaardwaarde, bijvoorbeeld 's' voor stop
-int speed = 0;
+char direction;
+int speed;
+
+unsigned long startupTime = millis();
 
 unsigned long startMillis = 0;
 unsigned long lastMessageMillis = 0;
 
 unsigned long lastBatteryPublishMillis = 0;
-const long batteryPublishInterval = 1000;
+const long batteryPublishInterval = 10000;
 
 WiFiManager wifiManager(WIFI_SSID, WIFI_PASSWORD, LED_PIN);
 MQTTManager mqttManager(espClient, MQTT_SERVER, MQTT_PORT, MQTT_USER, MQTT_PASSWORD, MQTT_CLIENTID.c_str());
 MotorDriver motorDriver(MOTOR1_ENA, MOTOR1_IN1, MOTOR1_IN2, MOTOR2_IN3, MOTOR2_IN4, MOTOR2_ENB);
 
-int readBatteryLevel()
+float getBatteryVoltage()
 {
-  int analogValue = analogRead(BATTERY_PIN);
-  float voltage = (analogValue / 4095.0) * 3.3;
-  float batteryVoltage = voltage / (R2 / (R1 + R2));
-  float level = map(batteryVoltage, 0, BATTERY_MAX_VOLTAGE, 1, 5);
-  return level;
+  int adcValue = analogRead(BATTERY_PIN);
+  float batteryVoltage = (adcValue / ADC_RESOLUTION) * 3.3 * VOLTAGE_DIVIDER;
+  return batteryVoltage;
+}
+
+int getBatteryPercentage(float batteryVoltage)
+{
+  int batteryPercentage = (batteryVoltage - BATTERY_MIN_VOLTAGE) / (BATTERY_MAX_VOLTAGE - BATTERY_MIN_VOLTAGE) * 100;
+  batteryPercentage = constrain(batteryPercentage, 0, 100);
+  return batteryPercentage;
+}
+
+int getUpTime()
+{
+  unsigned long currentTime = millis();
+  unsigned long runningTime = (currentTime - startupTime) / 1000;
+  return runningTime;
+}
+
+int getTimeLeft(int batteryPercentage)
+{
+  return batteryPercentage * MINUTES_DRIVING_PER_PERCENTAGE;
 }
 
 void updateMotorControl(char direction, int speed)
@@ -98,7 +121,6 @@ void updateMotorControl(char direction, int speed)
 
 void callbackMQTT(char *topic, byte *payload, unsigned int length)
 {
-  newMQTTMessage = true;
   mqttManager.printMessage(topic, payload, length);
 
   if (strcmp(topic, "bally/directions") == 0)
@@ -126,6 +148,8 @@ void setup()
   mqttManager.setCallback(callbackMQTT);
   mqttManager.connect();
   mqttManager.subscribe("bally/directions");
+
+  motorDriver.setup();
 }
 
 void loop()
@@ -139,13 +163,19 @@ void loop()
     }
     mqttManager.loop();
 
-    int batteryLevel = readBatteryLevel();
+    float batteryVoltage = getBatteryVoltage();
+    int batteryPercentage = getBatteryPercentage(batteryVoltage);
+    int upTime = getUpTime();
+    int timeLeft = getTimeLeft(batteryPercentage);
 
     unsigned long currentMillis = millis();
     if (currentMillis - lastBatteryPublishMillis >= batteryPublishInterval)
     {
       DynamicJsonDocument doc(1024);
-      doc["battery"] = batteryLevel;
+      doc["voltage"] = batteryVoltage;
+      doc["percentage"] = batteryPercentage;
+      doc["uptime"] = upTime;
+      doc["timeleft"] = timeLeft;
 
       // Serialize JSON object to String
       String payload;
@@ -160,7 +190,7 @@ void loop()
 
     clearDisplay();
     updateMotorControl(direction, speed);
-    drawBatteryIndicator(88, batteryLevel, "Bat.");
+    drawBatteryIndicator(88, map(batteryPercentage, 0, 100, 1, 5), "Bat.");
     showDisplay();
   }
 }
